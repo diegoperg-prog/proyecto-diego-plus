2590756bcc0e65c22531c12fc92b317359c955ca
diegoperg-prog <diego.perg@gmail.com>
2025-10-24 19:03:53 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "./", // ✅ rutas relativas
   build: {
     outDir: "dist",
     assetsDir: "assets",
-  },
+    emptyOutDir: true
+  }
 });
